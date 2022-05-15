64c8af5c7b5a1cb3798a90994d468805fb769d8d
NUR <ter.realbe.kan@gmail.com>
2022-05-15 21:29:33 +0700

    Update Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.18
+      <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="{{ asset('AdminLTE-2/https://adminlte.io') }}">AdminLTE</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2022 <a> KABINGO  </a>.</strong> by Nur Sarah
   </footer>
